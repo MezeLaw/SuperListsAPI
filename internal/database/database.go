d4539fd98da31d7bb72ade6406d4ddf281dc30c7
Meze <mezelaw@gmail.com>
2022-03-30 23:16:05 -0300

    Fix the docker-compose for wait postgres is ready before start app.

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,12 +3,20 @@ package database
 import (
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"os"
 )
 
 var AppDatabase *gorm.DB
 
 func InitDatabase() {
-	dsn := "host=localhost user=meze password=meze dbname=superlists port=5432 sslmode=disable"
+
+	PostgresUser := os.Getenv("POSTGRES_USER")
+	PostgresPassword := os.Getenv("POSTGRES_PASSWORD")
+	PostgresIp := os.Getenv("POSTGRES_IP")
+	PostgresPort := os.Getenv("POSTGRES_PORT")
+	PostgresDb := os.Getenv("POSTGRES_DB")
+
+	dsn := "host=" + PostgresIp + " user=" + PostgresUser + " password=" + PostgresPassword + " dbname=" + PostgresDb + " port=" + PostgresPort + " sslmode=disable"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
